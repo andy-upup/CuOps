3b3a449fc3adf3beab3078a4ab1c09a19e32a9df
andy <andy.cccc@qq.com>
2025-05-18 10:54:33 +0800

    refactor transpose with gtest

--- examples/transpose/transpose.cu
+++ examples/transpose/transpose.cu
@@ -1,5 +1,6 @@
 #include <cuda.h>
 #include <cuda_runtime.h>
+#include <gtest/gtest.h>
 #include <stdlib.h>
 
 #include <cstdio>
@@ -10,7 +11,7 @@
 #include "transpose.cuh"
 #include "utils.hpp"
 
-int main() {
+TEST(Transpose, TransposeNaive) {
   constexpr int src_height = 2048;
   constexpr int src_width = 512;
   constexpr int num_iter = 5;
@@ -59,5 +60,111 @@ int main() {
   cudaFree(d_output);
   free(output);
   free(golden);
-  return 0;
+}
+
+TEST(Transpose, TransposeSmem) {
+  constexpr int src_height = 2048;
+  constexpr int src_width = 512;
+  constexpr int num_iter = 5;
+
+  constexpr int num_data = src_height * src_width;
+  float* input = (float*)malloc(num_data * sizeof(float));
+  float* d_input;
+  cudaMalloc((void**)&d_input, num_data * sizeof(float));
+
+  float* output = (float*)malloc(num_data * sizeof(float));
+  float* d_output;
+  cudaMalloc((void**)&d_output, num_data * sizeof(float));
+  float* golden = (float*)malloc(num_data * sizeof(float));
+
+  for (int i = 0; i < num_data; ++i) {
+    input[i] = 2.0 * (float)drand48() - 1.0;
+  }
+
+  for (int i = 0; i < src_height; ++i) {
+    for (int j = 0; j < src_width; ++j) {
+      golden[j * src_height + i] = input[i * src_width + j];
+    }
+  }
+
+  cudaMemcpy(d_input, input, num_data * sizeof(float), cudaMemcpyHostToDevice);
+
+  for (int i = 0; i < num_iter; ++i) {
+    perf::Perf perf("transpose_smem");
+    transpose::TransposeSmem(d_input, d_output, src_height, src_width);
+    cudaDeviceSynchronize();
+  }
+
+  cudaMemcpy(output, d_output, num_data * sizeof(float),
+             cudaMemcpyDeviceToHost);
+  if (check(output, golden, num_data)) {
+    printf("Output is right.\n");
+  } else {
+    printf("Output is wrong!\n");
+    for (int i = 0; i < num_data; ++i) {
+      printf("%f, %f", output[i], golden[i]);
+    }
+    printf("\n");
+  }
+
+  cudaFree(d_input);
+  cudaFree(d_output);
+  free(output);
+  free(golden);
+}
+
+TEST(Transpose, TransposeTile) {
+  constexpr int src_height = 2048;
+  constexpr int src_width = 512;
+  constexpr int num_iter = 5;
+
+  constexpr int num_data = src_height * src_width;
+  float* input = (float*)malloc(num_data * sizeof(float));
+  float* d_input;
+  cudaMalloc((void**)&d_input, num_data * sizeof(float));
+
+  float* output = (float*)malloc(num_data * sizeof(float));
+  float* d_output;
+  cudaMalloc((void**)&d_output, num_data * sizeof(float));
+  float* golden = (float*)malloc(num_data * sizeof(float));
+
+  for (int i = 0; i < num_data; ++i) {
+    input[i] = 2.0 * (float)drand48() - 1.0;
+  }
+
+  for (int i = 0; i < src_height; ++i) {
+    for (int j = 0; j < src_width; ++j) {
+      golden[j * src_height + i] = input[i * src_width + j];
+    }
+  }
+
+  cudaMemcpy(d_input, input, num_data * sizeof(float), cudaMemcpyHostToDevice);
+
+  for (int i = 0; i < num_iter; ++i) {
+    perf::Perf perf("transpose_tile4x4");
+    transpose::TransposeTile(d_input, d_output, src_height, src_width);
+    cudaDeviceSynchronize();
+  }
+
+  cudaMemcpy(output, d_output, num_data * sizeof(float),
+             cudaMemcpyDeviceToHost);
+  if (check(output, golden, num_data)) {
+    printf("Output is right.\n");
+  } else {
+    printf("Output is wrong!\n");
+    for (int i = 0; i < num_data; ++i) {
+      printf("%lf", output[i]);
+    }
+    printf("\n");
+  }
+
+  cudaFree(d_input);
+  cudaFree(d_output);
+  free(output);
+  free(golden);
+}
+
+int main(int argc, char** argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
